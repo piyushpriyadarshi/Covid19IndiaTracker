01333c7271f130f3ff8aa5fba150a632a91a0356
Piyush Priyadarshi <priyadarship85@gmail.com>
2020-04-19 20:24:28 +0530

    created ServletInitializer.java file inorder to support war file generation

---
+++ src/main/java/com/piyushpriyadarshi/covid19mail/ServletInitializer.java
@@ -0,0 +1,11 @@
+package com.piyushpriyadarshi.covid19mail;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Covid19mailApplication.class);
+    }
+}
